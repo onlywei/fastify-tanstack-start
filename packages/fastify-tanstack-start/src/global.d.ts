89097f94160e86cf69c4ece7cc6fb32ada72b867
Wei Wang <wei@fastmail.com>
2025-10-19 16:56:41 -0400

    Restore hoist settings to attempt to fix ci

--- packages/fastify-tanstack-start/src/global.d.ts
+++ packages/fastify-tanstack-start/src/global.d.ts
@@ -1,22 +1,3 @@
-// This file manually declares the @fastify/middie type augmentation
-// because TypeScript is not consistently loading it from the package itself,
-// especially in CI environments with pnpm workspaces (hoist: false).
-//
-// This is based on @fastify/middie's type definitions:
-// https://github.com/fastify/middie/blob/main/types/index.d.ts
-
-import type { IncomingMessage, ServerResponse } from 'node:http';
-
-declare module 'fastify' {
-	interface FastifyInstance {
-		use(fn: (req: IncomingMessage, res: ServerResponse, next: (err?: Error) => void) => void): this;
-		use(
-			route: string,
-			fn: (req: IncomingMessage, res: ServerResponse, next: (err?: Error) => void) => void,
-		): this;
-		use(
-			routes: string[],
-			fn: (req: IncomingMessage, res: ServerResponse, next: (err?: Error) => void) => void,
-		): this;
-	}
-}
+// This file ensures @fastify/middie type augmentations are loaded
+// See: https://github.com/fastify/middie#typescript-support
+import '@fastify/middie';
